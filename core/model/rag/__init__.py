5838cbd7678b69bd193234017ba807262dd5a540
yexi <fanguojun066@gmail.com>
2025-12-20 04:59:11 +0800

    1

--- core/model/rag/__init__.py
+++ core/model/rag/__init__.py
@@ -1,3 +1,3 @@
 from .siliconflow_embedding import SiliconFlowEmbedding
-from .utils import read_txt_file,split_text,intelligent_split
-__all__=["SiliconFlowEmbedding","read_txt_file","split_text","intelligent_split"]
+from .utils import read_txt_file,split_text,intelligent_split,async_process_pipeline
+__all__=["SiliconFlowEmbedding","read_txt_file","split_text","intelligent_split","async_process_pipeline"]
--- manage.py
+++ manage.py
@@ -1,8 +1,25 @@
-from config import EmbeddingConfig
-import httpx
-import aiofiles
-from core.model.rag import SiliconFlowEmbedding,read_txt_file,split_text,intelligent_split
 import asyncio
+import logging
+
+import httpx
+
+from config import EmbeddingConfig
+from core.model.rag import (
+    SiliconFlowEmbedding,
+    async_process_pipeline,
+    intelligent_split,
+    read_txt_file,
+    split_text,
+)
+
+# 配置日志
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.StreamHandler(),  # 输出到终端
+    ],
+)
 
 if __name__ == "__main__":
     embedding_config = EmbeddingConfig(
@@ -18,13 +35,18 @@ if __name__ == "__main__":
 
     async def main():
         file_str = input("输入路径")
+        storage_path = input("输入储存路径")
         combined_text = await read_txt_file(file_str)
-        result = split_text(combined_text)
-        chunks = intelligent_split(text=result, chunk_size=200)
-        file_path="debug.txt"
-        async with aiofiles.open(file_path,"w",encoding="utf-8") as f:
-            for c in chunks:
-                await f.write(f"{c}\n\n")
+        result = await asyncio.to_thread(split_text, text=combined_text)
+        chunks = await asyncio.to_thread(intelligent_split, text=result, chunk_size=200)
+        await async_process_pipeline(
+            chunks=chunks,
+            max_lines=5,
+            minute=1000,
+            consumer_count=1000,
+            siliconflow_embedding=se,
+            folder_path=storage_path,
+            model=embedding_config.model_name,
+        )
 
     asyncio.run(main())
-    
